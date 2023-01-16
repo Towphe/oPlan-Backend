17bf3ea95dd5e2865fe638cfc945e4cc11c6c1c0
Christopher James Castillo <castillochristopherjames@gmail.com>
2023-01-16 22:26:41 +0800

    Setup documentation

--- oPlan-API/Program.cs
+++ oPlan-API/Program.cs
@@ -4,12 +4,33 @@ using oPlan_Repository;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "A ASP.NET Core Web API that manages todo entries of many users."
+    });
+});
+builder.Services.AddControllers();
 builder.Services.AddDbContext<OPlanDbContext>(opts =>
 {
     opts.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
 });
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseHttpsRedirection();
 app.Run();
