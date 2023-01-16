b7502d492d6b4908209f4e21373bdfa0344cb83e
Christopher James Castillo <castillochristopherjames@gmail.com>
2023-01-16 21:13:47 +0800

    Configured DB on API project

--- oPlan-API/Program.cs
+++ oPlan-API/Program.cs
@@ -1,4 +1,14 @@
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using oPlan_Repository;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<OPlanDbContext>(opts =>
+{
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Database"));
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();
--- oPlan-Repository/OPlanDbContext.cs
+++ oPlan-Repository/OPlanDbContext.cs
@@ -20,11 +20,6 @@ public partial class OPlanDbContext : DbContext
     public virtual DbSet<Todo> Todos { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=oPlan-DB;Username=postgres;Password=pingu");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Agenda>(entity =>
